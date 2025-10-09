f59b9ea13c78bd2d2e53312a7f2185a035d5b0cb
Archa Vivek <72131930+Thorcha-Errox@users.noreply.github.com>
2025-10-09 07:13:21 +0530

    Initial Commit

---
+++ Leetcode/MergeSortedArray-88.cpp
@@ -0,0 +1,14 @@
+// problem no. 88
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i=m, j=0, k=0;
+        while(i<m+n && j<n){
+            if(nums1[i]==0){
+                nums1.at(i++) = nums2[j++];
+            }
+        }
+        sort(nums1.begin(), nums1.end());
+    }
+};
