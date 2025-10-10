6e3f8b31b94dae376961295d4e35265df954da04
Archa Vivek <72131930+Thorcha-Errox@users.noreply.github.com>
2025-10-10 07:43:03 +0530

    Updated Code

--- Leetcode/MoveZeros-283.cpp
+++ Leetcode/MoveZeros-283.cpp
@@ -19,6 +19,15 @@ public:
             }
         cnt--;
         }
+
+        //Two Pointer
+        int lastNonZero = 0;
+        for(int i=0;i<n;i++){
+            if(nums[i] != 0){
+                swap(nums[lastNonZero], nums[i]);
+                lastNonZero++;
+            }
+        }
         
     }
 };
