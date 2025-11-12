5f7dc35175e2223595d28d1a51f4a8a2867a87c9
Archa Vivek <72131930+Thorcha-Errox@users.noreply.github.com>
2025-11-12 23:24:25 +0530

    Updated

---
+++ Leetcode/Squares of a Sorted Array-977.cpp
@@ -0,0 +1,15 @@
+//Problem No:- 977
+// BRute Force Approch 
+
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        vector<int> ans;
+        for(int i=0; i<nums.size(); i++){
+            int square = nums[i] * nums[i];
+            ans.push_back(square);
+        }
+        sort(ans.begin(), ans.end());
+        return ans;
+    }
+};
