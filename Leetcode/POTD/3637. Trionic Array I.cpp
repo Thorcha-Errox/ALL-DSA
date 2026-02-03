ccbc6b44105c488b965e43c1e78f80c5172e2727
Archa Vivek <72131930+Thorcha-Errox@users.noreply.github.com>
2026-02-03 13:38:20 +0530

    Initial Commit

---
+++ Leetcode/POTD/3637. Trionic Array I.cpp
@@ -0,0 +1,54 @@
+// 3637. Trionic Array I
+
+// You are given an integer array nums of length n.
+// An array is trionic if there exist indices 0 < p < q < n − 1 such that:
+// nums[0...p] is strictly increasing,
+// nums[p...q] is strictly decreasing,
+// nums[q...n − 1] is strictly increasing.
+// Return true if nums is trionic, otherwise return false.
+
+// Example 1:
+// Input: nums = [1,3,5,4,2,6]
+// Output: true
+// Explanation:
+// Pick p = 2, q = 4:
+// nums[0...2] = [1, 3, 5] is strictly increasing (1 < 3 < 5).
+// nums[2...4] = [5, 4, 2] is strictly decreasing (5 > 4 > 2).
+// nums[4...5] = [2, 6] is strictly increasing (2 < 6).
+
+// Example 2:
+// Input: nums = [2,1,3]
+// Output: false
+// Explanation:
+// There is no way to pick p and q to form the required three segments.
+
+// Constraints:
+// 3 <= n <= 100
+// -1000 <= nums[i] <= 1000
+
+class Solution {
+public:
+    bool isTrionic(vector<int>& nums) {
+        int n = nums.size();
+        bool inc = false;
+        bool dec = false;
+        bool inc2 = false;
+        if(n < 4){
+            return false;
+        }
+        int i = 0;
+        while (i < n - 1 && nums[i] < nums[i + 1]) {
+            inc = true;
+            i++;
+        }
+        while (i < n - 1 && nums[i] > nums[i + 1]) {
+            dec = true;
+            i++;
+        }
+        while (i < n - 1 && nums[i] < nums[i + 1]) {
+            inc2 = true;
+            i++;
+        }
+        return inc && dec && inc2 && i == n-1;
+    }
+};
