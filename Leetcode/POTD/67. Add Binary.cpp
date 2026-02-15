e2d4f517ab330d2778aab8abc3845332fcb4ffae
Archa Vivek <72131930+Thorcha-Errox@users.noreply.github.com>
2026-02-15 21:57:09 +0530

    Added New File

---
+++ Leetcode/POTD/67. Add Binary.cpp
@@ -0,0 +1,34 @@
+// Problem No:- 67. Add Binary
+
+class Solution {
+public:
+    string addBinary(string a, string b) {
+
+        int n = a.length() - 1;
+        int m = b.length() - 1;
+        int carry = 0;
+        int sum = 0;
+        string result = "";
+
+        while (n >= 0 || m >= 0) {
+            sum = carry;
+
+            if (n >= 0) {
+                sum += a[n] - '0';
+                n--;
+            }
+            if (m >= 0) {
+                sum += b[m] - '0';
+                m--;
+            }
+            result.push_back((sum % 2 == 0) ? '0' : '1');
+            carry = (sum > 1) ? 1 : 0;
+        }
+
+        if (carry > 0) {
+            result.push_back('1');
+        }
+        reverse(begin(result), end(result));
+        return result;
+    }
+};
